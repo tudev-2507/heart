6bb33f2fb14e0e770f911f5d67f57b90c2bb6c52
Phạm Trường <59144820+pamtrg@users.noreply.github.com>
2022-05-12 23:45:17 +0700

    Create

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'You';
+const giftUrl = 'https://www.facebook.com/pamtrg/';
+const eventName = 'Chuyển năng lượng';
+const titleCard = 'Tặng đằng ấy';
+const contentCard = '“Có điều gì Me có thể làm để khiến You cảm thấy tốt hơn không? Chỉ cần nói từ đó.” <3.';
+
+
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
